6621e3b932f02392bb9c94cd04aa93ec5e95a19b
eddy-kayla <eduardo_alcan@hotmail.com>
2025-05-03 10:45:01 -0300

    fix: corrige configuração do Firebase com strings válidas

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,14 @@
 // src/firebase.js
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY",
-  authDomain: "SEU_DOMINIO.firebaseapp.com",
-  projectId: "SEU_PROJECT_ID",
-  storageBucket: "SEU_PROJECT_ID.appspot.com",
-  messagingSenderId: "SEU_SENDER_ID",
-  appId: "SUA_APP_ID"
+  apiKey: "AIzaSyAvZE3X3H-IDW3aABL-qyMzPnHuIr50nuA",
+  authDomain: "alphaboard-f7030.firebaseapp.com",
+  projectId: "alphaboard-f7030",
+  storageBucket: "alphaboard-f7030.appspot.com",
+  messagingSenderId: "1065401324777",
+  appId: "1:1065401324777:web:e38f9bcbad8c46e6ef8c78"
 };
 
 const app = initializeApp(firebaseConfig);
