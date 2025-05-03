bcde3b58dd736a0b0bfa69bb6fa87e5dbffee94e
eddy-kayla <eduardo_alcan@hotmail.com>
2025-05-03 12:43:48 -0300

    Update ReadingAndCourses.jsx

--- src/components/ReadingAndCourses.jsx
+++ src/components/ReadingAndCourses.jsx
@@ -1,6 +1,6 @@
 // src/ReadingAndCourses.jsx
 import React, { useState, useEffect } from 'react';
-import { db } from './firebase';
+import { db } from './firebase.js';
 import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 function ReadingAndCourses() {
