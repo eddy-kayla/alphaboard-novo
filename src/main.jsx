9a698fdebbc51688d277ceab94ac6481baf976c5
eddy-kayla <eduardo_alcan@hotmail.com>
2025-05-04 08:07:11 -0300

    Corrige postcss para rodar com module type

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,12 @@
-import './index.css';
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+// src/main.jsx
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css'; // Já carrega Tailwind
+import './App.css';   // Aqui é onde você garante os ajustes finos
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
